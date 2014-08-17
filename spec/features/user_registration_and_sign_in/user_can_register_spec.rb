f3faa196997d78e4da5ad6adc0821b90a87f9988
Asjed (Azi) Hussain <asjed.hussain@gmail.com>
2014-08-17 19:44:26 -0400

    Add registration tests

--- spec/features/user_registration_and_sign_in/user_can_register_spec.rb
+++ spec/features/user_registration_and_sign_in/user_can_register_spec.rb
@@ -18,4 +18,26 @@ feature "user can register" do
     expect(page).to have_content "Welcome! You have signed up successfully."
   end
 
+  it "does not create a new user if no info is inputted" do
+    user = FactoryGirl.build(:user)
+
+    visit new_user_registration_path
+    click_on "Sign me up!"
+
+    count = page.body.scan("can't be blank").count
+    expect(count).to equal(3)
+  end
+
+  it "does not create a new user if password confirmation is incorrrect" do
+    user = FactoryGirl.build(:user)
+
+    visit new_user_registration_path
+
+    fill_in "Email", with: user.email
+    fill_in "Name", with: user.name
+    fill_in "Password", with: user.password
+    click_on "Sign me up!"
+
+    expect(page).to have_content("doesn't match Password")
+  end
 end
