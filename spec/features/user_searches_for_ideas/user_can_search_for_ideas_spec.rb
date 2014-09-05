207f66a8622923967949eee50b6ac5ce08acc2bb
Asjed (Azi) Hussain <asjed.hussain@gmail.com>
2014-09-05 13:28:15 -0400

    Add search form to nav bar

--- spec/features/user_searches_for_ideas/user_can_search_for_ideas_spec.rb
+++ spec/features/user_searches_for_ideas/user_can_search_for_ideas_spec.rb
@@ -1,17 +1,14 @@
 require "rails_helper"
 
 feature "user can search for idea" do
-
   it "searches for idea name when user types in title" do
     ideas_in_search = []
     ["foo", "fool", "foolish"].each do |title|
       ideas_in_search << FactoryGirl.create(:idea, title: title)
     end
-
     ideas_not_in_search = FactoryGirl.create_list(:idea, 10)
 
     visit root_path
-
     fill_in "Search", with: "Foo"
     click_on "Search"
 
@@ -22,6 +19,4 @@ feature "user can search for idea" do
       expect(page).to_not have_content(idea.title)
     end
   end
-
-
 end
